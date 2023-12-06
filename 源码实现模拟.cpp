07e309bbf59fabd857f17808ca6b138460863f7d
Forcatch <1067543960@qq.com>
2023-12-06 23:29:00 +0800

    23/12/06

--- 源码实现模拟.cpp
+++ 源码实现模拟.cpp
@@ -9,7 +9,7 @@
 //
 //	return(eos - str - 1);
 //}
-int my_strlen(const char* str)
+static int my_strlen(const char* str)
 {
 	const char* p = str;
 	while (*p++);
@@ -49,4 +49,32 @@ int my_strcmp(const char* src, const char* dst)
 	while (!(ret == *p1 - *p2) && *p1)  //利用短路效应，
 		p1++, p2++;
 	return ret;
+}
+
+//char* __cdecl strcat(
+//	char* dst,
+//	const char* src
+//)
+//{
+//	char* cp = dst;
+//
+//	while (*cp)
+//		cp++;                   /* find end of dst */
+//
+//	while (*cp++ = *src++);       /* Copy src to end of dst */
+//
+//	return(dst);                  /* return dst */
+//
+//}
+static char* my_strcat(char* str1, const char* str2)
+{
+	char* p = str1;
+	while (*p++ != '\0');  //这里可以源码没有放在条件判断里，不需要p--一下
+	p--;
+	while (*str2 != 0)    //*p++=*str2++ 这里巧用了条件判断里会多加一次，正好可以把str2结尾的空字符也赋值过去
+	{
+		*p++ = *str2++;
+	}
+	*p = '\0';
+	return str1;
 }
